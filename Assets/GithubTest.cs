5d6bd759bcdd3b915757d10096fcef9b0fa647bf
tls123456798 <tlsgotjd1588@naver.com>
2025-09-10 10:23:16 +0900

    202509101022_Init
    깃허브 연동 확인 테스트

---
+++ Assets/GithubTest.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GithubTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // 안녕하세요. 테스트입니다.
+        // 지금은 깃허브 연동 테스트 중 입니다.
+        // 연동애 잘 되기를
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
